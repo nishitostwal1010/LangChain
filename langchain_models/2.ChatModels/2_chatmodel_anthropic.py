dbb706708811ca3da773d712753cb80b90ead933
nishitostwal1010 <157974203+nishitostwal1010@users.noreply.github.com>
2025-05-19 22:01:06 +0530

    Add files via upload

---
+++ langchain_models/2.ChatModels/2_chatmodel_anthropic.py
@@ -0,0 +1,12 @@
+# I don't have credits for it.
+
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatAnthropic(model='claude-3-5-sonnet-20241022')
+
+result = model.invoke("What is the capital of India")
+
+print(result)
---
+++ langchain_models/2.ChatModels/5_chatmodel_huggingface_local.py
@@ -0,0 +1,19 @@
+# In this code the model is downloaded and stored in your memory
+# then the input is passed we can use os library to change the place where the model cache is being stored.
+
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+    task='text-generation',
+    pipeline_kwargs=dict(
+        temperature=0.5,
+        max_new_tokens=100
+    )
+)
+
+model = ChatHuggingFace(llm=llm)
+
+result = model.invoke("What is the capital of India")
+
+print(result.content)
